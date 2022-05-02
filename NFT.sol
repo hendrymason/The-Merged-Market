71b7f02fef472aa3dc3b7334eae46ecfafbcd7d0
mheez <68663997+hendrymason@users.noreply.github.com>
2022-05-02 12:26:31 -0500

    Use this as main NFT contract before upgrade

--- NFT.sol
+++ NFT.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+// This is the base contract for the NFT  use as MVP to make sure it all works before upgrading to NFT_2.0
+
 import "./utils/ERC721/ERC721URIStorage.sol";
 
 pragma solidity >=0.7.0 <0.9.0;
